b6428220bcb2135baea1f4099c043ce76e28a59f
AlexVanchov <alex.vanchov@gmail.com>
2020-02-11 11:35:47 +0200

    Added Offer calss with foreign key to Category

---
+++ Data/MiddleMan.Data.Models/Offer.cs
@@ -0,0 +1,41 @@
+﻿using MiddleMan.Data.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MiddleMan.Data.Models
+{
+    public class Offer : BaseDeletableModel<int>
+    {
+        public Offer()
+        {
+            this.CreatedOn = DateTime.UtcNow;
+            this.IsDeleted = false;
+            this.Id = Guid.NewGuid().ToString();
+            this.ModifiedOn = DateTime.UtcNow;
+        }
+
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0, 5000)]
+        public double Price { get; set; }
+
+        public string Description { get; set; }
+
+        public string PicUrl { get; set; }
+
+        [ForeignKey("Category")]
+        public string CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
