723cb1bd924f9dc902377f619655d3ab7a80ae0a
AlexVanchov <36050251+AlexVanchovRandomNums@users.noreply.github.com>
2020-02-22 00:11:59 +0200

    scaffold identity

---
+++ Web/MiddleMan.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MiddleMan.Data;
+using MiddleMan.Data.Models;
+
+[assembly: HostingStartup(typeof(MiddleMan.Web.Areas.Identity.IdentityHostingStartup))]
+namespace MiddleMan.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
