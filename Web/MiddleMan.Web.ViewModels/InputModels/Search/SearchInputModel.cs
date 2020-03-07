e45c34b8b15bc3d9e5d0dd973ba9516b5dbd05f0
AlexVanchov <36050251+AlexVanchovRandomNums@users.noreply.github.com>
2020-03-07 13:42:20 +0200

    Search view

---
+++ Web/MiddleMan.Web.ViewModels/InputModels/Search/SearchInputModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MiddleMan.Web.ViewModels.Search
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class SearchInputModel
+    {
+        public string SearchWord { get; set; }
+    }
+}
