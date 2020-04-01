c46f35aeaa10dce4f8ca7ff96d572c3344a3bb2f
AlexVanchov <36050251+AlexVanchovRandomNums@users.noreply.github.com>
2020-04-01 17:24:49 +0200

    ViewComponent and Favorite design

---
+++ Web/MiddleMan.Web/ViewComponents/IndexOfferComponent.cs
@@ -0,0 +1,14 @@
+﻿namespace MiddleMan.Web.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+    using MiddleMan.Web.ViewModels.ViewModels.Offer;
+    using System.Threading.Tasks;
+
+    public class IndexOffer : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(OfferViewModel viewModel)
+        {
+            return this.View(viewModel);
+        }
+    }
+}
