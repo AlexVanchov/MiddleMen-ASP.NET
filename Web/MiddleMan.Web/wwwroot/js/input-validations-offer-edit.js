a16a0486731e2d974f34a5a9022d59311dfcd451
AlexVanchov <36050251+AlexVanchovRandomNums@users.noreply.github.com>
2020-04-15 10:49:58 +0300

    Edit offer prevent default bug fix

--- Web/MiddleMan.Web/wwwroot/js/input-validations-offer-edit.js
+++ Web/MiddleMan.Web/wwwroot/js/input-validations-offer-edit.js
@@ -1,17 +1,13 @@
 ﻿// input form in create offer page
 var btn = document.getElementById("submit-btn");
 btn.addEventListener("click", (e) => {
-    e.preventDefault();
 
     var img = document.getElementById("image-input").files[0];
 
-    if (img === undefined) {
-        document.getElementById("create-form").submit();
-    }
-    else if (img.size >= 1048576) {
-        alert("Image is too big");
-    }
-    else {
-        document.getElementById("create-form").submit();
+    if (img !== undefined) {
+        if (img.size >= 1048576) {
+            e.preventDefault();
+            alert("Image is too big");
+        }
     }
 });
